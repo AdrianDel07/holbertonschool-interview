bb1d4380ea3c429a565944aac4134c5d7376884c
ADeBu99 <adrian.asuncion@ceiba.com.co>
2021-08-12 23:09:43 -0500

    Adding Task Minimum Operations try#2

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,28 +4,16 @@ find fewest number of operations needed to result in
 exactly n H characters in the file.
 """
 
-import math
-
-
-def factors(n):
-    """factors of n number"""
-    mylist = []
-    while n % 2 == 0:
-        mylist.append(2)
-        n = n / 2
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            mylist.append(i)
-            n = n / i
-    if n > 2:
-        mylist.append(n)
-    return mylist
-
-
 def minOperations(n):
-    """calculate the minimum operations"""
-    if type(n) != int or n < 2:
+    """ Returns min operations to get n Hs. """
+
+    result = 0
+    index = 2
+    if n < 2:
         return 0
-    else:
-        numOperations = sum(factors(n))
-        return int(numOperations)
+    while (index < n + 1):
+        while n % index == 0:
+            result += index
+            n = n / index
+        index += 1
+    return result
